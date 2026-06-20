b52ed20537a064615c5d6a9731d72f6eda9011d4
Gabriel Dian de Oliveira <gabriel.dian@edu.pucrs.br>
2026-06-20 17:13:38 -0300

    Construtores e getters e setters de classes Servidor e Sensor criados e implementações da classe Sensor por suas subclasses
    Signed-off-by: Gabriel Dian de Oliveira <gabriel.dian@edu.pucrs.br>

--- src/sensor/SensorEnergia.java
+++ src/sensor/SensorEnergia.java
@@ -2,6 +2,10 @@ package sensor;
 
 public class SensorEnergia extends Sensor {
 
+	public SensorEnergia(int id, String tipo, String unidade, double leituraAtual, double limiteMinimo, double limiteMaximo) {
+		super(id, tipo, unidade, leituraAtual, limiteMinimo, limiteMaximo);
+	}
+
 	public double lerDado() {
 		return 0;
 	}
--- src/sensor/SensorMemoria.java
+++ src/sensor/SensorMemoria.java
@@ -2,6 +2,10 @@ package sensor;
 
 public class SensorMemoria extends Sensor {
 
+	public SensorMemoria(int id, String tipo, String unidade, double leituraAtual, double limiteMinimo, double limiteMaximo) {
+		super(id, tipo, unidade, leituraAtual, limiteMinimo, limiteMaximo);
+	}
+
 	public double lerDado() {
 		return 0;
 	}
