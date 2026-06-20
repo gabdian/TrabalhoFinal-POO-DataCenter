928c19775f32ac4a8e3d76db83fd1beeb9db1b29
Gabriel Dian de Oliveira <gabriel.dian@edu.pucrs.br>
2026-06-20 17:13:38 -0300

    Construtores e getters e setters de classes Servidor e Sensor criados e implementações da classe Sensor por suas subclasses
    Signed-off-by: Gabriel Dian de Oliveira <gabriel.dian@edu.pucrs.br>

--- src/sensor/Sensor.java
+++ src/sensor/Sensor.java
@@ -14,6 +14,63 @@ public abstract class Sensor {
 
 	private double limiteMaximo;
 
+	public Sensor(int id, String tipo, String unidade, double leituraAtual, double limiteMinimo, double limiteMaximo) {
+		this.id = id;
+		this.tipo = tipo;
+		this.unidade = unidade;
+		this.leituraAtual = leituraAtual;
+		this.limiteMinimo = limiteMinimo;
+		this.limiteMaximo = limiteMaximo;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getUnidade() {
+		return unidade;
+	}
+
+	public void setUnidade(String unidade) {
+		this.unidade = unidade;
+	}
+
+	public double getLeituraAtual() {
+		return leituraAtual;
+	}
+
+	public void setLeituraAtual(double leituraAtual) {
+		this.leituraAtual = leituraAtual;
+	}
+
+	public double getLimiteMinimo() {
+		return limiteMinimo;
+	}
+
+	public void setLimiteMinimo(double limiteMinimo) {
+		this.limiteMinimo = limiteMinimo;
+	}
+
+	public double getLimiteMaximo() {
+		return limiteMaximo;
+	}
+
+	public void setLimiteMaximo(double limiteMaximo) {
+		this.limiteMaximo = limiteMaximo;
+	}
+
 	public abstract double lerDado();
 
 	public boolean verificarAlerta() {
